a59d546de805227ff7e58a6645cdf266045d74d5
er.vinit@gmail.com <er.vinit@gmail.com>
2023-04-19 10:54:47 -0400

    Added more program

--- src/main/java/org/example/arrays/PossiblePairSumForGivenNumberFromArray.java
+++ src/main/java/org/example/arrays/PossiblePairSumForGivenNumberFromArray.java
@@ -4,7 +4,7 @@ import java.util.HashSet;
 import java.util.Scanner;
 import java.util.Set;
 
-public class PossiblePairForNumberSumFromArray {
+public class PossiblePairSumForGivenNumberFromArray {
     public static void main(String[] args) {
         System.out.print("Given Array : {1,2,3,4,5,6,7,8,9}\n");
         System.out.print("Enter number for pair search : ");
