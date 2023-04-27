9a823f2b9539169dbb811903119ab31d7ff9f135
er.vinit@gmail.com <er.vinit@gmail.com>
2023-04-27 16:51:08 -0400

    add more program

---
+++ src/main/java/org/example/dataStructure/BubbleSort.java
@@ -0,0 +1,28 @@
+package org.example.dataStructure;
+
+import java.util.Arrays;
+
+public class BubbleSort {
+    public static void main(String[] args) {
+        int[] array = {7, 3, 5, 9, 2, 5, 1, 6, 8, 9};
+        int n = array.length;
+        int k = array.length;
+        for ( int i = 0 ; i < n ; i++ ) {
+            for ( int j = 0 ; j < k ; j++ ) {
+                if (i != j && array[i] < array[j]) {
+                    int temp = array[i]; //7
+                    array[i] = array[j]; //{3,7...}
+                    array[j] = temp;
+                }
+            }
+        }
+        System.out.print("Sorted array : "+ Arrays.toString(array));
+        //Time complexity
+        //first for :- 1+n+n=1+2n
+        //second for :- 1+n+n+n=1+3n
+        //1+2n+3n+6n^2=1+5n+6n^2
+        //6n^2+5n=5n(1n+1)=5n^2
+        //O(n^2)=Quadratic time
+    }
+}
+
