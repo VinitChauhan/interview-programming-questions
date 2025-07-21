6e89b3b899e25b7fed9def6af27f7d6a94a2e707
Vinit Chauhan <er.vinit@gmail.com>
2025-07-20 23:34:28 -0400

    updated

--- src/main/java/org/example/arrays/BiggestNumberFromArrayElements.java
+++ src/main/java/org/example/arrays/BiggestNumberFromArrayElements.java
@@ -6,7 +6,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-public class Test {
+public class BiggestNumberFromArrayElements {
     
     public static void main(String[] args) {
 //        String str = "test,rest";
