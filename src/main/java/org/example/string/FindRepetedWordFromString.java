7ad1a8daa17753a81f180742ea964863a36225be
Vinit Chauhan <er.vinit@gmail.com>
2025-08-02 10:00:43 -0400

    added more

---
+++ src/main/java/org/example/string/FindRepetedWordFromString.java
@@ -0,0 +1,22 @@
+package org.example.arrays;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class FindRepetedWordFromString {
+    public static void main(String[] args) {
+        String str = "test,rest";
+
+        Map<String, Integer> collect = Arrays.asList("test", "best", "rest", "sats", "rest", "test", "rest")
+                .stream()
+                .filter(s -> {
+                    String[] split = str.split(",");
+                    List<String> list = Arrays.stream(split).toList();
+                    return list.contains(s);
+                }).collect(Collectors.toMap(word -> word, word -> 1, Integer::sum));
+
+        collect.entrySet().forEach(System.out::println);
+    }
+}
